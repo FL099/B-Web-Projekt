4f33a4cd72cf5182ff8dcd9da96a5d9911489fce
Florian <florian@sixtl.at>
2021-12-01 17:28:55 +0100

    Login jetzt auch mit Vergleich mit der DB

--- src/main/java/com/example/projekt/controller/LoginController.java
+++ src/main/java/com/example/projekt/controller/LoginController.java
@@ -1,8 +1,18 @@
 package com.example.projekt.controller;
 
 import com.example.projekt.model.Auth;
+import com.example.projekt.model.User;
+import com.example.projekt.repository.UserRepository;
 import com.example.projekt.util.JwtUtil;
+
 import org.springframework.web.bind.annotation.*;
+//import org.hibernate.search.jpa.FullTextEntityManager;
+//import org.hibernate.search.jpa.FullTextQuery;
+//import org.hibernate.search.jpa.Search;
+
+import javax.persistence.NoResultException;
+
+import java.util.List;
 
 import static com.example.projekt.util.crypt.getSHA256;
 import static com.example.projekt.util.crypt.isValid;
@@ -11,6 +21,12 @@ import static com.example.projekt.util.crypt.isValid;
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*")
 public class LoginController {
+
+    private UserRepository userRepository;
+
+    public LoginController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 
     @GetMapping
     public @ResponseBody String index(){
@@ -23,6 +39,45 @@ public class LoginController {
     }
 
     @PostMapping
+    public String test2CheckLogin(@RequestBody Auth auth){
+        String newHash = getSHA256(auth.getPassword());
+
+        String token = JwtUtil.generateToken(auth);
+
+
+        User user = null;
+
+        try {
+            for (User u : userRepository.findByEmailContaining(auth.getEmail())) {
+                System.out.println(u.getPassword()+ ".....");
+                user = u;
+
+            }
+
+            //zum testen statt DB Abfrage hashwert statt email
+            String vglWert = user.getPassword();
+
+            if (isValid(vglWert, newHash)){
+                //return "Login erfolgreich \n eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1heCBNdXN0ZXJtYW5uIiwiaWF0IjoxNjM4MDM5MDIyfQ.tjA107F7gW21ImFN0XHTxPgruG2iNqr-8z99byBjji0";
+                return token;
+            }else {
+                return "Login fehlgeschlagen: falsche Username/Passwort Kombination  \n" + newHash + "\n" + vglWert;
+            }
+            /*String hqlString = "SELECT us.email, us.password FROM user us WHERE us.email ='"+ auth.getEmail()+ "'";
+            Query hqlQuery = session.createQuery(hqlString);
+            List resultSet = hqlQuery.list();
+            String PWquery = "SELECT us.password FROM Users us";
+            user = (User) entityManager.createQuery("from User u where u.username = :username")
+                    .setParameter("username", auth.getEmail())
+                    .getSingleResult();*/
+        } catch (NoResultException e)  {
+            user = null;
+        }
+
+        return "Something went wrong";
+    }
+
+    @PostMapping("/test")
     public String checkLogin(@RequestBody Auth auth){
         String newHash = getSHA256(auth.getPassword());
 
@@ -40,21 +95,5 @@ public class LoginController {
 
     }
 
-    @PostMapping("/test")
-    public String testCheckLogin(@RequestBody Auth auth){
-        String newHash = getSHA256(auth.getPassword());
 
-        String token = JwtUtil.generateToken(auth);
-
-        //zum testen statt DB Abfrage hashwert statt email
-        String vglWert = auth.getEmail();
-
-        if (isValid(vglWert, newHash)){
-            //return "Login erfolgreich \n eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1heCBNdXN0ZXJtYW5uIiwiaWF0IjoxNjM4MDM5MDIyfQ.tjA107F7gW21ImFN0XHTxPgruG2iNqr-8z99byBjji0";
-            return token;
-        }else {
-            return "Login fehlgeschlagen: falsche Username/Passwort Kombination  \n" + newHash + "\n" + vglWert;
-        }
-
-    }
 }
--- src/main/java/com/example/projekt/repository/UserRepository.java
+++ src/main/java/com/example/projekt/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.projekt.repository;
 import com.example.projekt.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    List<User> findByEmailContaining(String email);
 }
